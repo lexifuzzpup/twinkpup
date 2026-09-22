27bd66602ce8f793022d2001e86745e95cc9bf4b
lexifuzzpup <lexifuzzpup@gmail.com>
2026-09-22 00:47:44 -0500

    Add posts

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,16 @@ getDb().run(`CREATE TABLE IF NOT EXISTS visits(
     id INT AUTO_INCREMENT PRIMARY KEY,
     time TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 )`);
+getDb().run(`CREATE TABLE IF NOT EXISTS users(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR2(50) UNIQUE CHECK(LENGTH(name) BETWEEN 4 AND 50)
+)`);
+getDb().run(`CREATE TABLE IF NOT EXISTS posts(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    time TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    author INT REFERENCES users(id),
+    content VARCHAR2(1000)
+)`);
 
 const server = serve({
     development: developmentEnabled,
@@ -22,7 +32,31 @@ const server = serve({
                 const db = getDb();
                 db.run("INSERT INTO visits(time) VALUES(CURRENT_TIMESTAMP)");
 
-                return Response.json(db.query("SELECT COUNT(time) AS visits FROM visits").get(1));
+                return Response.json(db.query("SELECT COUNT(time) AS visits FROM visits").get());
+            }
+        },
+        "/api/posts": {
+            async GET() {
+                const db = getDb();
+
+                const select = db.query(`
+                    SELECT id, UNIXEPOCH(time) AS time, author, content
+                    FROM posts
+                    ORDER BY time DESC
+                    LIMIT 20
+                `);
+                return Response.json(select.all());
+            },
+            async POST(req) {
+                const db = getDb();
+                const { content } = await req.json() as { content: string };
+                const insert = db.query(`
+                    INSERT INTO posts(time, author, content)
+                    VALUES(CURRENT_TIMESTAMP, NULL, $content)
+                `);
+                insert.run({ $content: content });
+                
+                return Response.json({ success: true });
             }
         }
     }
--- src/public/home/main.ts
+++ src/public/home/main.ts
@@ -1,8 +1,87 @@
-const visitCounter = document.querySelector("#visit-count");
+const visitCounter = document.querySelector("#visit-count") as HTMLSpanElement;
+const newPostForm = document.querySelector("#new-post") as HTMLFormElement;
+const recentPosts = document.querySelector("#recent-posts") as HTMLDivElement;
 
 main();
 
+interface PostCard {
+    id: number;
+    author: string;
+    time: number;
+    content: string;
+}
+
 async function main() {
+    try {
+        await loadVisitCounter()
+    } catch(e) {
+        console.error("Failed to load visit counter:", e);
+    }
+    try {
+        await loadRecentPosts()
+    } catch(e) {
+        console.error("Failed to load recent posts:", e);
+    }
+
+    if(newPostForm) {
+        newPostForm.addEventListener("submit", async event => {
+            event.preventDefault();
+            const formData = new FormData(newPostForm);
+
+            const content = formData.get("content") as string;
+
+            await fetch("/api/posts", { method: "POST", body: JSON.stringify({ content }) });
+            document.location.reload();
+        })
+    }
+}
+
+function createPostCard(post: PostCard) {
+    const container = document.createElement("div");
+    container.classList.add("post");
+
+    const author = document.createElement("span");
+    if(post.author == null) {
+        author.textContent = "Anonymous";
+        author.style.fontStyle = "oblique";
+    } else {
+        author.textContent = post.author;
+    }
+    author.classList.add("author");
+
+    const timestamp = document.createElement("span");
+    timestamp.textContent = new Date(post.time * 1000).toLocaleDateString(navigator.language, {
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "2-digit",
+        minute: "2-digit"
+    });
+    timestamp.classList.add("timestamp");
+
+    const content = document.createElement("code");
+    content.textContent = post.content;
+
+    container.append(author, timestamp, content);
+    return container;
+}
+
+async function loadRecentPosts() {
+    if(!recentPosts) return;
+
+    const request = await fetch("/api/posts");
+    if(request.ok) {
+        const posts = await request.json();
+
+        for(const post of posts) {
+            const card = createPostCard(post);
+
+            recentPosts.append(card);
+        }
+    }
+}
+
+async function loadVisitCounter() {
     if(!visitCounter) return;
 
     const request = await fetch("/api/visit", { method: "POST" });
